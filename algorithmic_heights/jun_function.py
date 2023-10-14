8045ebc3b96dc64aba4a35b92828cdb16fd8ca9b
Junqiang Xu <xujunqiang_0607@126.com>
2023-10-14 11:13:14 +0800

    fibo

---
+++ algorithmic_heights/jun_function.py
@@ -0,0 +1,16 @@
+def read_data(data_path):
+    '''
+    singe sequence
+    '''
+    with open(data_path, 'r') as f:
+        seq = f.read()
+        f.close()
+    return seq
+
+def gen_txt(result):
+    '''
+    generate result to txt
+    '''
+    f = open('output.txt', 'w')
+    f.write(result)
+    f.close()
